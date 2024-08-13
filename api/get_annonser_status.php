6bced912737c094a1410f9ceb912e98380d4ec88
anoteng <andreas@noteng.no>
2024-08-13 11:21:46 +0200

    New API endpoint ot get annonser_kalender status

---
+++ api/get_annonser_status.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of TSNF Vaktliste.
+ *
+ * TSNF Vaktliste is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TSNF Vaktliste is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TSNF Vaktliste. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+
+
+require '../config.php';
+//require '../auth.php';
+//
+//if (!isset($_SESSION['email'])) {
+//    http_response_code(401);
+//    echo json_encode(["message" => "Unauthorized"]);
+//    exit();
+//}
+header('Content-Type: application/json');
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+$data = null;
+if($id){
+    $query = "SELECT a.annonsert_kalender AS annonsert_kalender WHERE a.id = ?";
+    $result = $conn->query($query);
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+echo json_encode($data);
+?>
